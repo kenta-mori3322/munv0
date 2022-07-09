0619a4df1270f4034f192248526c79a53ad9b6df
VigorousDeveloper <dev.mania.639@gmail.com>
2022-07-09 21:11:17 +0800

    change clipboard to javascript code

--- vue/src/composables/useClipboard.ts
+++ vue/src/composables/useClipboard.ts
@@ -10,12 +10,22 @@ export default function (options?: ClipboardOptions) {
   async function copy(value: string | ComputedRef<string>) {
     
     // if (isSupported) {
-      await navigator.clipboard.writeText(unref(value))
-      hasCopied.value = true
-      console.log("copied!")
+      // await navigator.clipboard.writeText(unref(value))
+      // hasCopied.value = true
+      // console.log("copied!")
 
-      setTimeout(() => (hasCopied.value = false), options?.resetAfter || 1000)
+      // setTimeout(() => (hasCopied.value = false), options?.resetAfter || 1000)
     // }
+    
+    const elem = document.createElement('textarea');
+    elem.value = unref(value);
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+    hasCopied.value = true
+
+    setTimeout(() => (hasCopied.value = false), options?.resetAfter || 1000)
   }
   
   return {
